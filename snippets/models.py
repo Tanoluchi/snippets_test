4b57ee6151842ea996410b3f77e43f7684ba33c8
luciano valenzuela <luciano.valenzuela@t-mobs.com>
2025-03-22 20:51:03 -0300

    feat: add choice to the name field with the lexers name

--- snippets/models.py
+++ snippets/models.py
@@ -4,18 +4,25 @@ from django.contrib.auth.models import User
 from django.db import models
 
 from pygments import formatters, highlight, lexers
+from pygments.lexers import get_all_lexers
+from pygments.util import ClassNotFound
 
 
 class Language(models.Model):
-    name = models.CharField(max_length=50)
+    LEXERS_NAMES = ((lexer[1][0], lexer[0]) for lexer in get_all_lexers() if lexer[1]) # Use a generator to reduce memory
+
+    name = models.CharField(max_length=50, choices=LEXERS_NAMES)
     slug = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
         return self.name
     
     def get_lexer(self):
-        return lexers.get_lexer_by_name(self.slug)
-
+        try:
+            return lexers.get_lexer_by_name(self.name)
+        except ClassNotFound:
+            # Return "text" if not found the lexer
+            return lexers.get_lexer_by_name("text")
 
 class Snippet(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
